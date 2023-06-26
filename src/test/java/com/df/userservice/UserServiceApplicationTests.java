b61ba0db2ffe0d6dd9683164d4b8f48f12bd3af0
qoqme <rovnayainna@mail.ru>
2023-06-27 00:34:43 +0300

    Initial commit

---
+++ src/test/java/com/df/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.df.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
